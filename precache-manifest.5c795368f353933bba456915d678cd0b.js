9d5400b175f37773778a9bfa73129b0cd5198bcf
Mica <maricamolder@gmail.com>
2021-03-19 13:31:34 -0400

    first commit

---
+++ precache-manifest.5c795368f353933bba456915d678cd0b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b22b223173e754f13d8ba3994cade0c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58a19b5e32047f040ede",
+    "url": "/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "38ee9743466fec49091b",
+    "url": "/static/css/main.0ac1ac59.chunk.css"
+  },
+  {
+    "revision": "58a19b5e32047f040ede",
+    "url": "/static/js/2.d8d1b766.chunk.js"
+  },
+  {
+    "revision": "34491c6de1fbcf2388878b87785c74e7",
+    "url": "/static/js/2.d8d1b766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38ee9743466fec49091b",
+    "url": "/static/js/main.f049a84a.chunk.js"
+  },
+  {
+    "revision": "f7bdf0cfc9ea0dd44285692948c2d84f",
+    "url": "/static/js/main.f049a84a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d1948da6512e239c4cd",
+    "url": "/static/js/runtime-main.f9fa5b6e.js"
+  },
+  {
+    "revision": "4fbe107698b5bc6596db77df12b759b7",
+    "url": "/static/media/marc.4fbe1076.jpg"
+  },
+  {
+    "revision": "f384e97520ec575e9758eb5c77d4c634",
+    "url": "/static/media/sidebar-1.f384e975.jpg"
+  },
+  {
+    "revision": "59967c1ec954119b13914b737ac0c851",
+    "url": "/static/media/sidebar-2.59967c1e.jpg"
+  },
+  {
+    "revision": "9f333c085d3e063c2c51f33245fd723c",
+    "url": "/static/media/sidebar-3.9f333c08.jpg"
+  },
+  {
+    "revision": "fb414eb51ca9ab6a3e338736989012a4",
+    "url": "/static/media/sidebar-4.fb414eb5.jpg"
+  }
+]);
